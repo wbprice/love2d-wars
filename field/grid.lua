278d43b585304d8dcdf7f9b3f16c82a706c8e3da
Blaine Price <1wbprice@gmail.com>
2017-10-24 13:05:58 -0400

    generalize grid.lua

--- field/grid.lua
+++ field/grid.lua
@@ -5,17 +5,23 @@ function Grid.new()
   local self = setmetatable({}, Grid)
   self.vCells = 12
   self.hCells = 16
+  self.hOffset = 16
+  self.vOffset = 12
   return self
 end
 
-function Grid.draw(self)
+function Grid:draw()
   love.graphics.setColor(155, 155, 155, 255)
   for i=0,self.vCells do
-    love.graphics.line(16, i * 48 + 12, 784, i * 48 + 12)
+    love.graphics.line(self.hOffset, i * 48 + self.vOffset, 800 - self.hOffset, i * 48 + self.vOffset)
   end
   for j=0,self.hCells do
-    love.graphics.line(j * 48 + 16, 12, j * 48 + 16, 588)
+    love.graphics.line(j * 48 + self.hOffset, self.vOffset, j * 48 + self.hOffset, 600 - self.hOffset)
   end
+end
+
+function Grid:drawEntity(entity)
+  
 end
 
 return Grid
