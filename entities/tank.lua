ee74ed35eae2ec929de6c3c4a9053874cededb38
Blaine Price <1wbprice@gmail.com>
2017-11-10 11:25:49 -0500

    draw T on tank, color it red

--- entities/tank.lua
+++ entities/tank.lua
@@ -11,6 +11,8 @@ end
 function Tank:draw(posX, posY)
     love.graphics.setColor(255, 0, 0, 255)
     love.graphics.rectangle('fill', posX, posY, 48, 48)
+    love.graphics.setColor(255, 255, 255, 255)
+    love.graphics.print("T", posX + 13, posY + 5, 0, 3, 3)
 end
 
 return Tank
