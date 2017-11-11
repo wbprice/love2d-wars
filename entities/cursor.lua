462a040a503e9b3ab83c6798be7b70d2ae7684cd
Blaine Price <1wbprice@gmail.com>
2017-11-11 11:26:25 -0500

    Use a keymap to organize cursor movements. Don't bind cursor listener every update cycle

--- entities/cursor.lua
+++ entities/cursor.lua
@@ -8,43 +8,57 @@ function Cursor:draw(posX, posY)
   love.graphics.rectangle('line', posX, posY, 48, 48)
 end
 
+local moveSound = function()
+    moveSound:play()
+end
+
+function moveUp(self)
+    moveSound()
+    if self.y > 0 then
+      self.y = self.y - 1
+    end
+end
+
+function moveDown(self)
+    moveSound()
+    if self.y < 11 then
+      self.y = self.y + 1
+    end
+end
+
+function moveLeft(self)
+    moveSound()
+    if self.x > 0 then
+      self.x = self.x - 1
+    end
+end
+
+function moveRight(self)
+    moveSound()
+    if self.x < 15 then
+      self.x = self.x + 1
+    end
+end
+
+function onSelect(self)
+    selectSound:play()
+end
+
+local keymap = {
+    ['up'] = moveUp,
+    ['down'] = moveDown,
+    ['left'] = moveLeft,
+    ['right'] = moveRight,
+    ['z'] = onSelect,
+}
+
 function Cursor:onKeyPress()
     function love.keypressed(key, scancode, isRepeat)
         if isRepeat then
             return
         end
 
-        if scancode == 'up' then
-            moveSound:play()
-            if self.y > 0 then
-              self.y = self.y - 1
-            end
-        end
-
-        if scancode == 'down' then
-            moveSound:play()
-            if self.y < 11 then
-              self.y = self.y + 1
-            end
-        end
-
-        if scancode == 'left' then
-            moveSound:play()
-            if self.x > 0 then
-              self.x = self.x - 1
-            end
-        end
-
-        if love.keyboard.isDown('right') then
-            moveSound:play()
-            if self.x < 15 then
-              self.x = self.x + 1
-            end
-        end
-
-        if love.keyboard.isDown('z') then
-            selectSound:play()
-        end
+        return keymap[scancode](self)
     end
 end
 
